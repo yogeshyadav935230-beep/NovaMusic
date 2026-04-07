4e2cf800cf3f9097863a71f79bad6cd74cc80f30
yogeshyadav935230-beep <yogeshyadav935230@gmail.com>
2026-04-08 00:34:08 +0530

    Create RepositoryModule.kt

---
+++ app/src/main/java/com/nova/music/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.nova.music.di
+
+import com.nova.music.data.repository.MusicRepositoryImpl
+import com.nova.music.domain.repository.MusicRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindMusicRepository(
+        musicRepositoryImpl: MusicRepositoryImpl
+    ): MusicRepository
+}
